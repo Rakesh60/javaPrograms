64117f4d3b8bd37fd6431e6a468cbb9685fb42a7
Rakesh60 <rakeshhupta5353@gmail.com>
2024-05-14 17:12:39 +0530

    check data type of variable

--- TypeCast.java
+++ TypeCast.java
@@ -1,12 +1,21 @@
-public class TypeCast {
-    
+public class Main {
     public static void main(String[] args) {
-        // float fvar=23.45f;
-        // double dvar=fvar;
+        int a = 2;
+        String str = String.valueOf(a);
 
-        // System.out.println(fvar);
-        // System.out.println(dvar);
-        //*Advance type Casting */
+        System.out.println(a);           // Prints: 2
+        System.out.println(str);         // Prints: 2
+        System.out.println(a + a);       // Prints: 4 (since a is an int, it's an arithmetic addition)
+        System.out.println(str + str);   // Prints: 22 (since str is a String, it's string concatenation)
 
+        // To find the data type of a, we infer it from the declaration (it's an int)
+        // For str, we can use getClass().getName()
+        System.out.println(((Object) a).getClass().getName()); // This will not work as a is a primitive
+
+        System.out.println(str.getClass().getName());          // Prints: java.lang.String
+
+        // An alternative for int is using wrapper class
+        Integer wrapperA = a;
+        System.out.println(wrapperA.getClass().getName());     // Prints: java.lang.Integer
     }
 }
