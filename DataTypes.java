98f77e1f8765d6120cc1ea503b4529cf31d5fdbb
Rakesh60 <rakeshhupta5353@gmail.com>
2024-05-14 16:48:36 +0530

    java Practise

---
+++ DataTypes.java
@@ -0,0 +1,7 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // integer data types
+        int var=20;
+        System.out.println(var);
+    }
+}
---
+++ TypeCast.java
@@ -0,0 +1,12 @@
+public class TypeCast {
+    
+    public static void main(String[] args) {
+        // float fvar=23.45f;
+        // double dvar=fvar;
+
+        // System.out.println(fvar);
+        // System.out.println(dvar);
+        //*Advance type Casting */
+
+    }
+}
