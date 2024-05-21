cb1ea2f9989e73c77367f6ffc2aed94487b27df7
Rakesh60 <rakeshhupta5353@gmail.com>
2024-05-21 14:01:33 +0530

    if else

---
+++ Userinput.java
@@ -0,0 +1,31 @@
+public class Userinput {
+    public static void main(String[] args) {
+        //*ASsignment,Arithematic,increment-decrement operators */
+        //System.out.println("Hello world");
+        int a=10;
+        int b=20;
+        //post increment and pre increment
+        // System.out.println(a++);
+        // System.out.println(++a);
+        //post decrement and pre decrement
+        // System.out.println(b);
+        // --b;
+        // System.out.println(b);
+        // System.out.println(b--);
+        // System.out.println(b);
+        int rank=75;
+        int marks=1310;
+       if (rank<=70 || marks>=1310 ) {
+        System.out.println("In top 70's");
+       } else {
+        System.out.println("not in 70");
+       }
+        
+
+
+       if (a>b) {
+        
+       }
+
+    }
+}
