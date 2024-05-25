3473a5bb281bf5ef858a04515cf13f8079af0d25
Rakesh60 <rakeshgupta5353@gmail.com>
2024-05-25 12:26:01 +0530

    arary in java

---
+++ arrayUserInput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arrayUserInput {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the size of array");
+        int size = scan.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter values ");
+            arr[i] = scan.nextInt();
+        }
+
+        for (int i : arr) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
