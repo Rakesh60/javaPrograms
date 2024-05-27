f6ed841ddbdb430a8ea1c5faa9edcfe5b8396da6
Rakesh60 <rakeshhupta5353@gmail.com>
2024-05-27 16:41:35 +0530

    classes and object

---
+++ Classes.java
@@ -0,0 +1,14 @@
+class Rakesh{
+    int age=20;
+    String name="Rakesh";
+}
+
+
+
+
+
+public class Classes {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Methods.java
@@ -0,0 +1,13 @@
+
+public class Methods {
+
+
+   static void hello(){
+        System.out.println("Hello world");
+    }
+public static void main(String[] args) {
+    hello();
+    
+}
+    
+}
