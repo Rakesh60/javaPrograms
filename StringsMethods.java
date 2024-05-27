df961f0bf5e5b49d71c3852d822cf4f91dcaac70
Rakesh60 <rakeshgupta5353@gmail.com>
2024-05-28 00:27:01 +0530

    string

---
+++ StringsMethods.java
@@ -0,0 +1,13 @@
+public class StringsMethods {
+    public static void main(String[] args) {
+        String str1 = "Rakesh";
+        String str2 = "Rakesh";
+        String str3 = new String("Rakesh");
+        // System.out.println(str1 == str2);
+        // System.out.println(str1.equals(str3));
+        str3 = "Rakeshgupta";
+        // System.out.println(str1);
+        // System.out.println(str1);
+        System.out.println(str3);
+    }
+}
