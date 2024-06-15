72454d320677445362de6124a475d33863855948
Rakesh60 <rakeshgupta5353@gmail.com>
2024-06-15 10:02:39 +0530

    collection

---
+++ collections/MyList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyList {
+    public static void main(String[] args) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        ls.add(10);
+        ls.add(20);
+        ls.add(30);
+        System.out.println(ls);
+
+        LinkedList<Integer> lklList = new LinkedList<Integer>();
+        lklList.add(55);
+        
+
+    }
+
+}
